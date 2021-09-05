f2c9c416fad61e0ce876999650169f181014a23e
Joseph Rivera <32499278+JoeyRivera01@users.noreply.github.com>
2021-09-05 16:21:38 -0700

    Time: 76 ms (95.19%), Space: 41.9 MB (31.96%) - LeetHub

---
+++ meeting-rooms-ii/meeting-rooms-ii.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var minMeetingRooms = function(intervals) {
+  if (!intervals.length) return 0;
+  intervals.sort((a,b)=> a[0] === b[0] ?  a[1] - b[1] : a[0] - b[0]);
+
+  let minHeap = [];
+  for (let i = 0; i < intervals.length; i++){
+    let [start, end] = intervals[i];
+    if (!minHeap.length) minHeap.push(end);
+    else{
+      let earliestAvailableTime = minHeap[0];
+      if (start < earliestAvailableTime) {
+        minHeap.push(end);
+        minHeap.sort((a,b)=>a>b ? 1 : -1);
+      }
+      else {
+        minHeap[0] = Math.max(minHeap[0], end);
+        minHeap.sort((a,b)=>a>b ? 1 : -1);
+      }
+    }
+  }
+  return minHeap.length;
+
+};
