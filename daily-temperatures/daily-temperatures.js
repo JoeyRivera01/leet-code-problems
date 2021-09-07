b4788cac00b9de39110c73fb15b73c191d8f6a8a
Joseph Rivera <32499278+JoeyRivera01@users.noreply.github.com>
2021-09-07 13:06:12 -0700

    Time: 268 ms (61.77%), Space: 61.1 MB (56.26%) - LeetHub

---
+++ daily-temperatures/daily-temperatures.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function(temperatures) {
+  // time complexity: O(n)
+  // space complexity: O(n)
+  let res = Array.from({length:temperatures.length}, x => 0);
+  let stack = [];
+  for (let i = 0; i < temperatures.length; i++) {
+    while (stack.length > 0 && temperatures[stack[stack.length - 1]] < temperatures[i]) {
+      let j = stack.pop();
+      res[j] = i - j;
+    }
+    stack.push(i)
+  }
+  return res;
+};
