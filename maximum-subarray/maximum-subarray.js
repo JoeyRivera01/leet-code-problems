ff99f38f2b1ba7971ca8852dfc335caeff1a2bf5
Joseph Rivera <32499278+JoeyRivera01@users.noreply.github.com>
2021-09-09 15:38:01 -0700

    Time: 80 ms (63.49%), Space: 40 MB (81.12%) - LeetHub

--- maximum-subarray/maximum-subarray.js
+++ maximum-subarray/maximum-subarray.js
@@ -2,9 +2,20 @@
  * @param {number[]} nums
  * @return {number}
  */
+
+// Time complexity: O(N), where N is the length of nums.
+// We iterate through every element of nums exactly once.
+// Space complexity: O(1)
+// No matter how long the input is, we are only ever using 2 variables: currentSubarray and maxSubarray.
 var maxSubArray = function(nums) {
-    for (let i = 1; i < nums.length; i++){
-        nums[i] = Math.max(nums[i], nums[i] + nums[i - 1]);
-    }
-    return Math.max(...nums);
+  let currentSubarray = nums[0];
+  let maxSubarray = nums[0];
+  
+  for (let i = 1; i < nums.length; i++){
+    let num = nums[i];
+    currentSubarray = Math.max(num, currentSubarray + num);
+    maxSubarray = Math.max(maxSubarray, currentSubarray);
+  }
+  
+  return maxSubarray;
 };
