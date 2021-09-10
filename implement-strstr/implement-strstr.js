522f67e58b1b2b14d3272f2c5160eb304d90ea1f
Joseph Rivera <32499278+JoeyRivera01@users.noreply.github.com>
2021-09-10 14:52:58 -0700

    Time: 150 ms (37.68%), Space: 40.4 MB (48.05%) - LeetHub

--- implement-strstr/implement-strstr.js
+++ implement-strstr/implement-strstr.js
@@ -3,21 +3,18 @@
  * @param {string} needle
  * @return {number}
  */
-const strStr = (haystack, needle) => {
-  if (needle === '' || needle === haystack) return 0;    // the only mandatory check here is (needle === '')
-  if (haystack.length < needle.length) return -1;        // the other ones are for efficiency
-  
-  for (let i = 0; i < haystack.length - needle.length + 1; i++) {    // start looping through haystack until the remaining substring is shorter than needle
-    if (haystack[i] === needle[0]) {                // as soon as we see a character that matches the first character of needle
-      for (let j = 0; j < needle.length; j++) {     // start looping through both needle and haystack
-        if (needle[j] !== haystack[i + j]) {        // as soon as the characters don't match
-          break;                                    // break out of the loop and return to looping through haystack
-        } else if (j === needle.length - 1){        // otherwise, if we looped through the entire needle and all of the characters matched
-          return i;                                 // return the index of the first character of haystack with which we started the loop
+var strStr = function(haystack, needle) {
+    // if needle is an empty string, return zero
+    if (needle.length === 0) return 0;
+    
+    // iterate over the haystack string
+    for (var i = 0; i < haystack.length; i++) {
+        if (haystack[i] === needle[0]) {
+           let substring = haystack.substring(i, i + needle.length);
+            if (substring === needle) {
+                return i;
+            }
         }
-      }
     }
-  }
-  
-  return -1;                                        // return -1 if there wasn't a match
+    return -1;
 };
