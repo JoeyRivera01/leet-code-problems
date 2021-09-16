28c371d8283376ff61f77199e6ed8ca6ed405045
Joseph Rivera <32499278+JoeyRivera01@users.noreply.github.com>
2021-09-16 13:44:04 -0700

    Time: 88 ms (27.63%), Space: 38.5 MB (97.40%) - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -3,45 +3,32 @@
  * @param {number} target
  * @return {number}
  */
-var search = function(nums, target) {
-  let left = 0;
-  let right = nums.length - 1;
-    
-  while (left <= right) {
-    let mid = Math.floor((left + right) / 2);
-    
-    if (nums[mid] === target) {
-      return mid;
-    }
-    
-    // When dividing the roated array into two halves, one must be sorted.
-    
-    // Check if the left side is sorted
-    if (nums[left] <= nums[mid]) {
-      if (nums[left] <= target && target <= nums[mid]) {
-        // target is in the left
-        right = mid - 1;
-        
-      } else {
-        // target is in the right
-        left = mid + 1;
-      }
-    } 
-    
-    // Otherwise, the right side is sorted
-    else {
-      if (nums[mid] <= target && target <= nums[right]) {
-        // target is in the right
-        left = mid + 1;
 
-      } else {
-        // target is in the left
-        right = mid - 1;
-      }
+const search = function(nums, target) {
+    let left = 0, right = nums.length - 1;
+    while (left <= right) {
+        let mid = Math.floor((right + left) / 2);
+        // base case
+        if (target === nums[mid]) {
+            return mid;
+        }
+        // check to see if the left side is sorted
+        if (nums[left] <= nums[mid]) {
+            // target is on the left
+            if (nums[left] <= target && target <= nums[mid]) {
+                right = mid - 1;
+            } else {
+                // target is on the right
+                left = mid + 1;
+            }
+        // otherwise, right array is sorted 
+        } else {
+            if (nums[mid] <= target && target <= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
     }
-    
-    
-  }
-    
-  return -1;
-};
+    return -1;
+}
