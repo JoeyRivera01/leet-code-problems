8dbf429b2f95d9508fe223f14c30b95bb3f6bb7e
Joseph Rivera <32499278+JoeyRivera01@users.noreply.github.com>
2021-09-24 19:33:27 -0700

    Time: 72 ms (92.13%), Space: 38.6 MB (96.80%) - LeetHub

---
+++ longest-common-prefix/longest-common-prefix.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+// time complexity: O(S) where S is the sum of all characters in the all strings
+// O(1) We only used constant extra space
+
+const longestCommonPrefix = (strs) => {
+    if (strs.length === 0) return '';
+    let prefix = strs[0];
+    for (let i = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.substring(0, prefix.length - 1);
+        }
+        if (prefix.length === 0) return '';
+    }
+    return prefix;
+}
