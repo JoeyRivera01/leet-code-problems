6356bf4f9b6f089ab2eda8cc798a04e42a34b86a
Joseph Rivera <32499278+JoeyRivera01@users.noreply.github.com>
2021-09-27 12:25:50 -0700

    Time: 116 ms (72.35%), Space: 51.4 MB (37.98%) - LeetHub

---
+++ product-of-array-except-self/product-of-array-except-self.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    let len = nums.length;
+    let answer = new Array(len);
+    
+    answer[0] = 1;
+    for (let i = 1; i < len; i++) {
+        answer[i] = nums[i - 1] * answer[i - 1];
+    }
+    
+    let r = 1;
+    for (let i = len - 1; i >= 0; i--) {
+        answer[i] = answer[i] * r;
+        r *= nums[i];
+    }
+    
+    return answer;
+};
