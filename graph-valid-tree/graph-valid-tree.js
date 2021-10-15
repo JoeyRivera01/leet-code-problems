ca663b22e3c1c726282607554496518995de2d43
Joseph Rivera <32499278+JoeyRivera01@users.noreply.github.com>
2021-10-15 14:48:03 -0700

    Time: 134 ms (21.15%), Space: 40.7 MB (79.59%) - LeetHub

--- graph-valid-tree/graph-valid-tree.js
+++ graph-valid-tree/graph-valid-tree.js
@@ -3,32 +3,35 @@
  * @param {number[][]} edges
  * @return {boolean}
  */
-var validTree = function(n, edges) {
-    const UnionFind = function(n){
+
+
+const validTree = (n, edges) => {
+    const DisjoinSet = function(n) {
         let parent = {};
         let size = {};
         this.count = n;
         
-        for (let i=0; i<n; i++){
-            parent[i]=i;
-            size[i]=1;
+        for (let i = 0; i < n; i++) {
+            parent[i] = i;
+            size[i] = 1;
         }
-
+        
         this.find = (x) => {
-            if (parent[x]===x) return x;
+            if (parent[x] === x) return x;
+            // use recursion to compress path
             parent[x] = this.find(parent[x]);
             return parent[x];
         }
-
-        this.union = (x,y) => {
+        
+        this.union = (x, y) => {
             let rootX = this.find(x);
             let rootY = this.find(y);
-
+            
             if (rootX === rootY) return false;
-            if (size[rootX] > size[rootY]){
+            if (size[rootX] > size[rootY]) {
                 size[rootX] += size[rootY];
                 parent[rootY] = rootX;
-            }else{
+            } else {
                 size[rootY] += size[rootX];
                 parent[rootX] = rootY;
             }
@@ -36,10 +39,9 @@ var validTree = function(n, edges) {
             return true;
         }
     }
-
-    const UF = new UnionFind(n);
-    for (let edge of edges){
-        if (!UF.union(edge[0], edge[1])) return false;
+    const ds = new DisjoinSet(n);
+    for (let edge of edges) {
+        if (!ds.union(edge[0], edge[1])) return false;
     }
-    return UF.count===1;
-};
+    return ds.count === 1;
+}
