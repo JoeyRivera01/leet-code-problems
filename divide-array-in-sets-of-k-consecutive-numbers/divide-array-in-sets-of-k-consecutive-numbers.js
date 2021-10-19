a97bc79813e47009cdbcbfba806876142c2b3ebc
Joseph Rivera <32499278+JoeyRivera01@users.noreply.github.com>
2021-10-19 12:26:20 -0700

    Time: 190 ms (85.71%), Space: 55.6 MB (84.13%) - LeetHub

--- divide-array-in-sets-of-k-consecutive-numbers/divide-array-in-sets-of-k-consecutive-numbers.js
+++ divide-array-in-sets-of-k-consecutive-numbers/divide-array-in-sets-of-k-consecutive-numbers.js
@@ -3,17 +3,30 @@
  * @param {number} k
  * @return {boolean}
  */
+
 const isPossibleDivide = (nums, k) => {
-    if(nums.length % k)return false
-    let map = {}, set = new Set(nums)
-    nums.forEach(x => map[x] ? map[x]++ : map[x] = 1)
-    let count = nums.length / k
-    while(count--){
-        let min = Math.min(...set)
-        for(let i = min; i < min + k; i++){
-            if(!map[i])return false
-            if(--map[i] == 0)set.delete(i)
-        }
+  if (nums.length % k !== 0) return false;
+  const map = new Map();
+  for (const val of nums) {
+    map.set(val, map.has(val) ? map.get(val) + 1 : 1);
+  }
+  for (let start of nums) {
+    if (map.get(start) === 0) continue;
+    while (map.get(--start) > 0);
+    ++start;
+    const count = map.get(start);
+    for (let i = 1; i < k; ++i) {
+      const c = map.get(start + i)
+      if (c === undefined || c < count) return false;
+      map.set(start + i, c - count);
     }
-    return true
+    map.set(start, 0);
+  }
+  return true;
 };
+
+
+
+
+
+
