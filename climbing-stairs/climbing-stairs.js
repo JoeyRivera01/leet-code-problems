ce230f21d22a245a1127819bb39489e4f536c6f5
Joseph Rivera <32499278+JoeyRivera01@users.noreply.github.com>
2021-10-21 12:11:23 -0700

    Time: 176 ms (5.16%), Space: 46.4 MB (5.68%) - LeetHub

--- climbing-stairs/climbing-stairs.js
+++ climbing-stairs/climbing-stairs.js
@@ -6,13 +6,20 @@ const climbStairs = function(n) {
         return 1;
     }
     
-    let arr = [n + 1];
-    arr[1] = 1;
-    arr[2] = 2;
+    let arr = [];
+    console.log(arr);
     
-    for (i = 3; i <= n; i++) {
+    arr[0] = 1;
+    arr[1] = 2;
+    console.log(arr);
+    
+    
+    for (let i = 2; i <= n; i++) {
         arr[i] = arr[i - 1] + arr[i - 2];
+        console.log(arr);  
     }
     
-    return arr[n];
+    return arr[n - 1];
 };
+
+climbStairs(30);
