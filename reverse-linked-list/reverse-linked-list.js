3642e5daf552898a38f6bd23b8afd049d441de4d
Joseph Rivera <32499278+JoeyRivera01@users.noreply.github.com>
2021-10-22 21:32:35 -0700

    Time: 134 ms (8.89%), Space: 40.3 MB (87.17%) - LeetHub

--- reverse-linked-list/reverse-linked-list.js
+++ reverse-linked-list/reverse-linked-list.js
@@ -9,10 +9,38 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-const reverseList = function(head) {
-  if (head === null || head.next === null) return head;
-  let p = reverseList(head.next);
-  head.next.next = head;
-  head.next = null;
-  return p;
-};
+
+const reverseList = (head) => {
+    let prev = null;
+    let curr = head;
+    while (curr !== null) {
+        let tmp = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = tmp;
+    }
+    return prev;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
