fe6a8285df33a2de3316d6a92c03b2c92a61d00a
Joseph Rivera <32499278+JoeyRivera01@users.noreply.github.com>
2022-09-01 15:44:13 -0700

    Time: 85 ms (69.18%), Space: 42.3 MB (76.85%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.js
+++ 14-longest-common-prefix/14-longest-common-prefix.js
@@ -3,13 +3,16 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    // approach horizontal scanning
-    let prefix = strs[0];
-    for (let i = 1; i < strs.length; i++) {
-        while (strs[i].indexOf(prefix) !== 0) {
-            prefix = prefix.substring(0, prefix.length - 1);
-            if (prefix === '') return '';
+    // approach vertical scanning
+    if (strs == null || strs.length == 0) return '';
+    for (let i = 0; i < strs[0].length; i++) {
+        let char = strs[0][i];
+        for (let j = 1; j < strs.length; j++) {
+            if (i === strs[j].length || strs[j][i] !== char) {
+                return strs[0].substring(0, i);
+            }
         }
     }
-    return prefix;
+    
+    return strs[0];
 };
