b369c944eb45db69a274cfd4030117e4e230741a
Joseph Rivera <32499278+JoeyRivera01@users.noreply.github.com>
2022-09-01 16:25:59 -0700

    Time: 78 ms (85.81%), Space: 44.1 MB (70.34%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
@@ -11,13 +11,21 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-  if(!list1 || !list2) return (list1? list1:list2);
-  if(list1.val < list2.val) {
-    list1.next = mergeTwoLists(list1.next, list2);
-    return list1;
-  } else {
-    list2.next = mergeTwoLists(list1, list2.next);
-    return list2;
-  }
+    if (list1 == null) return list2;
+    if (list2 == null) return list1;
+    
+    let head = new ListNode();
+    
+    if (list1.val < list2.val) {
+        head = list1;
+        list1 = list1.next;
+    } else {
+        head = list2;
+        list2 = list2.next;
+    }
+    
+    head.next = mergeTwoLists(list1, list2);
+        
+    return head;
 };
 
