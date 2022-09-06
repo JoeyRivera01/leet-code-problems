c878643e45b0342303f59c69814f0b0bb1e1a59e
Joseph Rivera <32499278+JoeyRivera01@users.noreply.github.com>
2022-09-05 21:42:28 -0700

    Time: 82 ms (91.99%), Space: 45.2 MB (70.19%) - LeetHub

--- 696-count-binary-substrings/696-count-binary-substrings.js
+++ 696-count-binary-substrings/696-count-binary-substrings.js
@@ -4,20 +4,9 @@
  */
 var countBinarySubstrings = function(s) {
     // approach - group by character
-    let groups = [];
-    let t = 0;
-    groups[0] = 1;
-    for (let i = 1; i < s.length; i++) {
-        if (s[i-1] !== s[i]) {
-            groups[++t] = 1;
-        } else {
-            groups[t]++;
-        }
-    }
-    
-    let ans = 0;
-    for (let i = 1; i <= t; i++) {
-        ans += Math.min(groups[i-1], groups[i]);
-    }
-    return ans;
+    let curr = 1, prev = 0, ans = 0
+    for (let i = 1; i < s.length; i++)
+        if (s[i] === s[i-1]) curr++;
+        else ans += Math.min(curr, prev), prev = curr, curr = 1;
+    return ans + Math.min(curr, prev)
 };
