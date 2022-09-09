1bda0d6ddae89f7ed5f5aa42aa4155ae47f54d72
Joseph Rivera <32499278+JoeyRivera01@users.noreply.github.com>
2022-09-09 12:02:17 -0700

    Time: 216 ms (94.26%), Space: 51.2 MB (58.18%) - LeetHub

--- 326-power-of-three/326-power-of-three.js
+++ 326-power-of-three/326-power-of-three.js
@@ -3,12 +3,5 @@
  * @return {boolean}
  */
 var isPowerOfThree = function(n) {
-    if (n === 0) return false;
-    
-    while (n % 3 === 0) {
-        n /= 3;
-    }
-    
-    return n === 1;
-    
+    return /^10*$/.test(n.toString(3));
 };
