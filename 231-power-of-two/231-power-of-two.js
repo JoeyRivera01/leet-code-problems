7e3091dec7870c24c634b21dd5a2ce390785aed8
Joseph Rivera <32499278+JoeyRivera01@users.noreply.github.com>
2022-09-09 11:41:34 -0700

    Time: 67 ms (98.30%), Space: 42.9 MB (91.12%) - LeetHub

--- 231-power-of-two/231-power-of-two.js
+++ 231-power-of-two/231-power-of-two.js
@@ -3,9 +3,5 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function(n) {
-    if (n === 0) return false;
-    while (n %2 === 0) {
-        n /= 2;
-    }
-    return n === 1;
+    return Number.isInteger(Math.log2(n));
 };
