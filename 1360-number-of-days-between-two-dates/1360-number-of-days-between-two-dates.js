de28f1e9af054edd8aca69cec5d7a817d4c0c8aa
Joseph Rivera <32499278+JoeyRivera01@users.noreply.github.com>
2022-09-14 21:37:18 -0700

    Time: 101 ms (28.03%), Space: 42.8 MB (6.37%) - LeetHub

--- 1360-number-of-days-between-two-dates/1360-number-of-days-between-two-dates.js
+++ 1360-number-of-days-between-two-dates/1360-number-of-days-between-two-dates.js
@@ -1,47 +1,10 @@
 var daysBetweenDates = function(date1, date2) {
-    const days1 = calculateDays(date1)
-    const days2 = calculateDays(date2)
-    
-    return Math.abs(days1 - days2)
+  if (date2 === date1) return 0;
+  
+  if (date2 > date1) {
+    console.log(new Date(date2) - new Date(date1))
+    return (new Date(date2) - new Date(date1)) / (1000 * 60 * 60 * 24);
+  }
+  
+  return (new Date(date1) - new Date(date2)) / (1000 * 60 * 60 * 24);
 };
-
-function calculateDays(date) {
-    const [y, m, d] = date.split("-")
-    
-    const yearDays = calculateDaysUpToYears(parseInt(y))
-    const monthDays = calculateDaysUpToMonths(parseInt(m), parseInt(y))
-    const dayDays = parseInt(d)
-    
-    const days = yearDays + monthDays + dayDays
-    
-    return days
-}
-
-function calculateDaysUpToYears(year) {
-    let days = 0
-    
-    for (let i = 1971; i < year; i++) {
-        days += (isLeapYear(i)) ? 366 : 365
-    }
-    
-    return days
-}
-
-function isLeapYear(year) {
-    return (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0);
-}
-
-function calculateDaysUpToMonths(month, year) {
-    const months = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]; // 0 is to take care of the month offset
-    let days = 0
-    
-    for (let i = 1; i < month; i++) {
-        days += months[i]
-    }
-    
-    if (month > 2 && isLeapYear(year)) {
-        days += 1
-    }
-    
-    return days
-}
