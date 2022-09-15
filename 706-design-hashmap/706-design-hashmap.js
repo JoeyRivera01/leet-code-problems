1bfe897cdb41239d8c755c914aa7ed25e4534b09
Joseph Rivera <32499278+JoeyRivera01@users.noreply.github.com>
2022-09-14 21:16:30 -0700

    Time: 403 ms (17.22%), Space: 56.7 MB (21.14%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.js
+++ 706-design-hashmap/706-design-hashmap.js
@@ -1,37 +1,39 @@
-
-var MyHashMap = function() {
-    this.data = new Array(1000001);
+class ListNode {
+    constructor(key, val, next) {
+        this.key = key
+        this.val = val
+        this.next = next
+    }
+}
+class MyHashMap {
+    constructor() {
+        this.size = 19997
+        this.mult = 12582917
+        this.data = new Array(this.size)
+    }
+    hash(key) {
+        return key * this.mult % this.size
+    }
+    put(key, val) {
+        this.remove(key)
+        let h = this.hash(key)
+        let node = new ListNode(key, val, this.data[h])
+        this.data[h] = node
+    }
+    get(key) {
+        let h = this.hash(key), node = this.data[h]
+        for (; node; node = node.next)
+            if (node.key === key) return node.val
+        return -1
+    }
+    remove(key) {
+        let h = this.hash(key), node = this.data[h]
+        if (!node) return
+        if (node.key === key) this.data[h] = node.next
+        else for (; node.next; node = node.next)
+            if (node.next.key === key) {
+                node.next = node.next.next
+                return
+            }
+    }
 };
-
-/** 
- * @param {number} key 
- * @param {number} value
- * @return {void}
- */
-MyHashMap.prototype.put = function(key, value) {
-    this.data[key] = value;
-};
-
-/** 
- * @param {number} key
- * @return {number}
- */
-MyHashMap.prototype.get = function(key) {
-    return this.data[key] != undefined ? this.data[key] : -1;
-};
-
-/** 
- * @param {number} key
- * @return {void}
- */
-MyHashMap.prototype.remove = function(key) {
-    delete this.data[key];
-};
-
-/** 
- * Your MyHashMap object will be instantiated and called as such:
- * var obj = new MyHashMap()
- * obj.put(key,value)
- * var param_2 = obj.get(key)
- * obj.remove(key)
- */
