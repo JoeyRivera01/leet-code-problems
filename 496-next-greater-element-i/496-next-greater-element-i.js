e7dd5a031a0eb0f7e38f2c995d01b21952a0e758
Joseph Rivera <32499278+JoeyRivera01@users.noreply.github.com>
2022-09-14 21:06:32 -0700

    Time: 120 ms (26.42%), Space: 44.5 MB (33.55%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.js
+++ 496-next-greater-element-i/496-next-greater-element-i.js
@@ -4,16 +4,21 @@
  * @return {number[]}
  */
 var nextGreaterElement = function(nums1, nums2) {
-    let ans = [];
-    nums1.forEach((num) => {
-        let idx = nums2.indexOf(num) + 1;
-        for (; idx < nums2.length; idx++) {
-            if (num < nums2[idx]) {
-                ans.push(nums2[idx]);
-                return;
-            }
+    let stack = [];
+    let greaterElMap = {};
+    
+    // pre-process nums2 into stack and map
+    for (let i = 0; i < nums2.length; i++) {
+        while (stack.length > 0 && nums2[i] > stack[stack.length - 1]) {
+            greaterElMap[stack.pop()] = nums2[i];
         }
-        ans.push(-1);
-    })
-    return ans;
+        stack.push(nums2[i]);
+    }
+    
+    // set any elements with no greater els to the right as -1
+    while (stack.length > 0) {
+        greaterElMap[stack.pop()] = -1;
+    }
+    
+    return nums1.map((num) => greaterElMap[num]);
 };
