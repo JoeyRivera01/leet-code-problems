971f739f578b5e1bf1f93c6811b922a5f5ef2e5c
Joseph Rivera <32499278+JoeyRivera01@users.noreply.github.com>
2022-09-14 21:37:22 -0700

    Time: 90 ms (49.05%), Space: 41.6 MB (89.17%) - LeetHub

--- 1360-number-of-days-between-two-dates/1360-number-of-days-between-two-dates.js
+++ 1360-number-of-days-between-two-dates/1360-number-of-days-between-two-dates.js
@@ -2,7 +2,6 @@ var daysBetweenDates = function(date1, date2) {
   if (date2 === date1) return 0;
   
   if (date2 > date1) {
-    console.log(new Date(date2) - new Date(date1))
     return (new Date(date2) - new Date(date1)) / (1000 * 60 * 60 * 24);
   }
   
